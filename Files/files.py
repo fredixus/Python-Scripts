2747f9fa107b543f0b160e88d3dbb7634506df75
Michal Debosz <michal.debosz2@sp.woodplc.com>
2022-05-18 13:49:46 +0200

    Add new function checkFolders-verify if folders exists.

--- Files/files.py
+++ Files/files.py
@@ -9,3 +9,11 @@ def getFilesInWorkingDir(workingPath):
     """
     filesInWorkingDir = [file for file in os.listdir(workingPath) if os.path.isfile(file)] 
     return filesInWorkingDir
+
+def checkFolders(path, subdir):
+    dirToCheck = f"{path}/{subdir}"
+    if(os.path.exists(dirToCheck)):
+        return True
+    else:
+        print(f"Error: {dirToCheck} not found")
+        return False
